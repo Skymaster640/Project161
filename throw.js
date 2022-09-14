004872c38a69d48822bc067a7a0f96da8b14ca50
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2022-09-13 20:46:36 -0500

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("bowlingball",{
+    init:function(){
+        this.throwball()
+    },
+    throwball:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key === "z"){
+                var ball = document.createElement("a-entity")
+                ball.setAttribute("gltf-model","#bowling_ball")
+                var cam = document.querySelector("#camera")
+                pos = cam.getAttribute("position")
+                ball.setAttribute("position",{
+                    x:pos.x,y:pos.y,z:pos.z
+                })
+                var camera = document.querySelector("#camera").object3D
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                ball.setAttribute("velocity",direction.multiplyScalar(-10))
+                var scene = document.querySelector("#scene")
+                scene.appendChild(ball)
+            }
+        })
+    }
+})
